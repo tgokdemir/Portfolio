d9a69efe9936b7b9fcda51ab50fcd1986dd2116b
Tunc Gokdemir <144594789+tgokdemir@users.noreply.github.com>
2023-10-15 21:40:44 +0200

    JavaScript>Iching_fortune added divination feature

--- JavaScript/Iching_fortune/iching.js
+++ JavaScript/Iching_fortune/iching.js
@@ -8,13 +8,69 @@ function genRandNum(num){
     return Math.floor(Math.random()*num);
 }
 
+// names and adjectives database for generating random names
 const namesAndAdjectives ={
-    names: ['alligator', 'fox', 'lion', 'wolf', 'deer', 'rabbit', 'machine'
-    , 'robot'],
-    adjectives1: ['beautifull','ugly','fast', 'excited','charming','irresistable', 
-        'sleeping'],
-    adjectives2: ['runner','fainter','liar','vibrant','Exquisite']
+    names: ['Alligator', 'Fox', 'Lion', 'Wolf', 'Deer', 'Rabbit', 'Machine'
+    , 'Robot'],
+    adjectives1: ['Beautifull','Ugly','Fast', 'Excited','Charming','Irresistable', 
+        'Sleeping'],
+    adjectives2: ['Runner','Fainter','Liar','Vibrant','Exquisite']
 }
+
+// I Ching hexagrams for divination
+const iChingHexagrams = [
+    {
+        number: 32,
+        name : 'Permanance',
+        lines : ' - -\n'+'  - -\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  - -'
+                        
+    }, {
+        number: 24,
+        name : 'Return',
+        lines : ' - -\n'+'  - -\n'+'  - -\n'+'  - -\n'+'  - -\n'+'  ---'
+                        
+    }, {
+        number: 4,
+        name : 'Ignorance',
+        lines : ' ---\n'+'  - -\n'+'  - -\n'+'  - -\n'+'  ---\n'+'  - -'
+                        
+    }, {
+        number: 1,
+        name : 'Sunrise',
+        lines : ' ---\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  ---'
+                        
+    }, {
+        number: 41,
+        name : 'Reduction',
+        lines : ' ---\n'+'  - -\n'+'  - -\n'+'  - -\n'+'  ---\n'+'  ---'
+                        
+    }, {
+        number: 11,
+        name : 'Passing Through',
+        lines : ' - -\n'+'  - -\n'+'  - -\n'+'  ---\n'+'  ---\n'+'  ---'
+                        
+    }, {
+        number: 33,
+        name : 'Withdrawal',
+        lines : ' ---\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  - -\n'+'  - -'
+                        
+    }, {
+        number: 28,
+        name : 'Too Much',
+        lines : ' - -\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  - -'
+                        
+    }, {
+        number: 43,
+        name : 'Deciding',
+        lines : ' - -\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  ---\n'+'  ---'
+                        
+    }, {
+        number: 9,
+        name : 'Minor Restraint',
+        lines : ' ---\n'+'  ---\n'+'  - -\n'+'  ---\n'+'  ---\n'+'  ---'
+                        
+    }
+]
 
 /* A factory function that generates random avatar name objects for 
 the user*/
@@ -30,6 +86,13 @@ function genRandName(){
 }
 
 
-const name1 =genRandName();
+// A function that makes an I ching divination
+function iChingDivination(){
+    let userName = genRandName();
+    let hexagram = iChingHexagrams[genRandNum(iChingHexagrams.length)];
+    console.log(`\nDear ${userName.fullName}, here are the results of your I ching consultation:\n${hexagram.number}. ${hexagram.name} \n Hexagram lines:\n ${hexagram.lines}`  )
+}
 
-console.log(name1.fullName);
+// Make a divination
+iChingDivination()
+
